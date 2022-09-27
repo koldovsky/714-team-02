06250166b1768c7872d465191d48cb857b0ac328
Leonid <leonidgmn@gmail.com>
2022-09-27 12:41:24 +0000

    add new file "membership-slider.js config file guides.js on Prittier

--- js/guides.js
+++ js/guides.js
@@ -1,39 +1,41 @@
-(function() {
-    
+(function () {
+  const guidesData = [
+    {
+      id: 0,
+      img: "img/ann-leroy-foto.jpg",
+      name: "Ann Leroy",
+      experience: "Tour Operator, 6 years of experience",
+      title:
+        "Ann joined the staff of Cilantro in 2012. She is an expert tour guide. She knows all the gems and the best sights to discover in France.",
+      languages: "English, French, German",
+    },
+    {
+      id: 1,
+      img: "img/paul-rouge-foto.jpg",
+      name: "Paul Rouge",
+      experience: "Senior Tour Agent, 10 years of experience",
+      title:
+        "Paul is our licensed senior tour agent with more than 10 years of travel industry experience under his belt.",
+      languages: "English, French, Italian",
+    },
+    {
+      id: 2,
+      img: "img/joyce-buckland-foto.jpg",
+      name: "Joyce Buckland",
+      experience: "Tour Guide, 4 years of experience",
+      title:
+        "Joyce joined our team as a travel guide. She is always very friendly and attentive to clients to help them get a memorable travel experience.",
+      languages: "English, French",
+    },
+  ];
 
-    const guidesData = [
-        {
-            id: 0,
-            img: "img/ann-leroy-foto.jpg",
-            name: "Ann Leroy",
-            experience: "Tour Operator, 6 years of experience",
-            title: "Ann joined the staff of Cilantro in 2012. She is an expert tour guide. She knows all the gems and the best sights to discover in France.",
-            languages: "English, French, German",
-        },
-        {
-            id: 1,
-            img: "img/paul-rouge-foto.jpg",
-            name: "Paul Rouge",
-            experience: "Senior Tour Agent, 10 years of experience",
-            title: "Paul is our licensed senior tour agent with more than 10 years of travel industry experience under his belt.",
-            languages: "English, French, Italian",
-        },
-        {
-            id: 2,
-            img: "img/joyce-buckland-foto.jpg",
-            name: "Joyce Buckland",
-            experience: "Tour Guide, 4 years of experience",
-            title: "Joyce joined our team as a travel guide. She is always very friendly and attentive to clients to help them get a memorable travel experience.",
-            languages: "English, French",
-        },
-    ];
-
-    
-    function renderGuideCard(guidesData) {
-        const cilantroGuidesCards = document.querySelector('.cilantro-guides__cards');
-        cilantroGuidesCards.innerHTML = '';
-        for (const card of guidesData) {
-            cilantroGuidesCards.innerHTML += `
+  function renderGuideCard(guidesData) {
+    const cilantroGuidesCards = document.querySelector(
+      ".cilantro-guides__cards"
+    );
+    cilantroGuidesCards.innerHTML = "";
+    for (const card of guidesData) {
+      cilantroGuidesCards.innerHTML += `
             <div class="guide-card">
               <div class="guide-card__header-footer">
                 <img
@@ -78,11 +80,9 @@
                 </div>
               </div>
             </div>
-            `
-        }
+            `;
     }
+  }
 
-    renderGuideCard(guidesData);
-
-
+  renderGuideCard(guidesData);
 })();
