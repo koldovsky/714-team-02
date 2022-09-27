16d423a5cea2272f3955e703f24e28d5c672fa37
mhevyk <maksimgevyk@gmail.com>
2022-09-27 23:12:33 +0300

    Wrapped burger.js code into iffe and improved code

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,14 @@
-const burgerIcon = document.querySelector(".burger__menu-icon");
-const body = document.body;
-const headerTopbar = document.querySelector(".header__topbar");
+(function() {
+    const body = document.body;
+    const burgerIcon = document.querySelector(".burger__menu-icon");
+    const headerTopbar = document.querySelector(".header__topbar");
+    //CSS class, that forces styles to change when being added
+    const burgerToggleClassName = "burger-active";
 
-//CSS class, that forces styles to change when being added
-const burgerToggleClassName = "burger-active";
+    function toggleBurgerMenu() {
+        body.classList.toggle(burgerToggleClassName);
+        headerTopbar.classList.toggle(burgerToggleClassName);
+    }
 
-burgerIcon.addEventListener("click", () => {
-    body.classList.toggle(burgerToggleClassName);
-    headerTopbar.classList.toggle(burgerToggleClassName);
-});
+    burgerIcon.addEventListener("click", toggleBurgerMenu);
+})();
