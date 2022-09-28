b42e801d48b17ed119b7d6b7c3d7f5f0109231c0
vikrur <62720717+vikrur@users.noreply.github.com>
2022-09-28 14:42:17 +0300

    clock added to gallery page

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,9 @@
-(function () {})();
+(function () {
+  
+  const clockContainer = document.querySelector(".gallery__clock-container");
+
+  function updateClock() {
+    clockContainer.innerHTML = new Date().toLocaleString();
+  }
+  setInterval(updateClock, 1000);
+})();
