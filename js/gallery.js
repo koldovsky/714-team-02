111b4fbc882a74b56d29c3994f5e7aab770df561
vikrur <62720717+vikrur@users.noreply.github.com>
2022-10-07 21:13:32 +0300

    clock on the gallery page and file gallery.js deleted

--- js/gallery.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  
-  const clockContainer = document.querySelector(".gallery__clock-container");
-
-  function updateClock() {
-    clockContainer.innerHTML = new Date().toLocaleString();
-  }
-  setInterval(updateClock, 1000);
-})();
