b06a4909345792c4a22be2e2f98f6012e7b09468
vikrur <62720717+vikrur@users.noreply.github.com>
2022-10-10 19:58:49 +0300

    Changed gallery page

--- js/gallery-carousel.js
+++ js/gallery-carousel.js
@@ -1,52 +1,91 @@
-let slideIndex = 0;
-showSlides(slideIndex);
+(function () {
+  // робим масив з картинок
+  const slides = [
+    `<article class="card">
+                    <img class="card__image" src="img/royal_palace_madrid.jpg" alt="Paris tour image">
+                    <h4 class="gallery__h4">Paris in spring</h4>
+                </article>`,
+    `<article class="card">
+                    <img class="card__image" src="img/luara_castle.jpeg" alt="Burgundy tour image">
+                    <h4 class="gallery__h4">Burgundy culture and wine</h4>
+                </article>`,
+    `<article class="card">
+                    <img class="card__image" src="img/hungarian_parliament.jpeg" alt="Budapest tour image">
+                    <h4 class="gallery__h4">Budapest parliament guided tours</h4>
+                </article>`,
+    `<article class="card">
+                    <img class="card__image" src="img/torre_de_belem.jpg" alt="Normandy tour image">
+                    <h4 class="gallery__h4">Normandy Tour</h4>
+                </article>`,
+    `<article class="card">
+                    <img class="card__image" src="img/paris_seine.jpg" alt="Paris in autumn tour image">
+                    <h4 class="gallery__h4">Paris in autumn</h4>
+                </article>`,
+    `<article class="card">
+                    <img class="card__image" src="img/gothic_buildings.jpg" alt="Paris tour image">
+                    <h4 class="gallery__h4">Charming Paris tour</h4>
+                </article>`,
+  ];
 
-// Next/previous controls
-function plusSlides(slideNumber) {
-  showSlides((slideIndex += slideNumber));
-}
+  // присвоюємо індекс 0 початковому слайду
+  let currentSlideIdx = 0;
 
-// Thumbnail image controls
-function currentSlide(slideNumber) {
-  showSlides((slideIndex = slideNumber));
-}
+  // обробник події на зміну ширини екрану (щоб адаптивно змінювалась кількість картинох, відображених в галереї)
+  window.addEventListener("resize", renderSlide);
 
-function showSlides(slideNumber) {
-  let slides = document.getElementsByClassName("card");
-  if (slideNumber > slides.length - 1) {
-    slideIndex = 0;
-  }
-  if (slideNumber < 0) {
-    slideIndex = slides.length - 1;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex].style.display = "grid";
+  // вибираєм кнопку некст
+  const btnNext = document.querySelector(".images-carousel .btn-next");
+  // коли клікаєм на кнопку, запускається функція, що міняє на наступний слайд
+  btnNext.addEventListener("click", nextSlide);
 
-  // два слайди на сторінку
-  if (window.innerWidth >= 767) {
-    let secondSlideIdx;
-    let slideIndexPlusOne = slideIndex + 1;
-    if (slideIndexPlusOne >= slides.length) {
-      secondSlideIdx = 0;
-    } else {
-      secondSlideIdx = slideIndex + 1;
-    }
-    slides[secondSlideIdx].style.display = "grid";
+  // вибираєм кнопку прев
+  const btnPrev = document.querySelector(".images-carousel .btn-prev");
+  // коли клікаєм на кнопку, запускається функція, що міняє на попередній слайд
+  btnPrev.addEventListener("click", prevSlide);
 
-    //три слайди на сторінку
-    if (window.innerWidth >= 991) {
-      let thirdSlideIdx;
-      let secondSlideIdxPlusOne = secondSlideIdx + 1;
-      if (secondSlideIdxPlusOne >= slides.length) {
-        thirdSlideIdx = 0;
-      } else {
-        thirdSlideIdx = secondSlideIdx + 1;
+  // вибираєм з html селектором діви де будуть наші слайди і присвоюємо перемінній
+  const slideContainer = document.querySelector(
+    ".images-carousel .carousel-slides"
+  );
+
+  // функція, що виводить слайди в html
+  function renderSlide() {
+    //беремо значення currentSlideIdx і вписуємо його в InnerHtml (початковий індекс масиву slides)
+    slideContainer.innerHTML = slides[currentSlideIdx];
+    // робимо декілька слайдів в залежності від ширини екрана
+
+    if (window.innerWidth >= 767) {
+      // два слайди на сторінку
+      const secondSlideIdx =
+        currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+      slideContainer.innerHTML += slides[secondSlideIdx];
+
+      //три слайди на сторінку
+      if (window.innerWidth >= 991) {
+        const thirdSlideIdx =
+          secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+        slideContainer.innerHTML += slides[thirdSlideIdx];
       }
-      slides[thirdSlideIdx].style.display = "grid";
     }
   }
-}
-// обробник події на зміну ширини екрану (щоб адаптивно змінювалась кількість картинох, відображених в галереї)
-window.addEventListener("resize", showSlides);
+
+  // функція що міняє слайди по натисненню на кнопочку = currentSlide збільнити на одичинку
+  function nextSlide() {
+    // перевіряєм чи це не останній по порядку слайд, щоб було куди далі лістати
+    currentSlideIdx =
+      currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
+    renderSlide();
+  }
+
+  function prevSlide() {
+    // перевіряєм чи попередній слайд мінусовий, якщо так, то
+    currentSlideIdx =
+      currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
+    renderSlide();
+  }
+
+  // викликаємо функцію, щоб js запрацював на сторінці і перший слайд вивевся і далі інші
+  renderSlide();
+
+  //   setInterval(nextSlide, 5000);
+})();
